807ea290fc86707c5254119b61c19956184a883b
liuyi1985 <liuyi1985@8b243031-8947-0410-b39a-2b519deb81bd>
2008-04-29 04:27:13 +0000

    made a copy

---
+++ branch/ocr/ocr/LibManager.h
@@ -0,0 +1,21 @@
+#ifndef _LIBMANAGER_H
+#define _LIBMANAGER_H
+
+namespace library{
+
+	class LibManager
+	{
+	public:
+		static void appendChars(const char *appendFilePath);
+
+	private:
+		static const wchar_t s_chUnicBegin = 0x4E00;
+		static const wchar_t s_chUnicEnd = 0x9FA5;
+
+		LibManager(void){  };
+		~LibManager(void){  };
+	};
+
+}
+
+#endif
